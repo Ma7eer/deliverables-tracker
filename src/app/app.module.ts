4ab57dffe26f96ba86187f959dbf4b5cd1e9fa5f
Ma7eer <maher.alkendi2@gmail.com>
2021-03-25 00:03:17 +0400

    Add table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatSliderModule} from '@angular/material/slider'
+import {MatTableModule} from '@angular/material/table';
+import { TableComponent } from './table/table.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
