b134af3a1d466f672a4b5e48a5190603c7776fe0
Ma7eer <maher.alkendi2@gmail.com>
2021-03-26 13:32:15 +0400

    Add routing to the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewProjectsComponent } from './view-projects/view-projects.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'projects', component: ViewProjectsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
