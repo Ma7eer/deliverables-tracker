749ea6bafd2336ad75dc9a1dfc4fbba2d599674c
Maher Alkendi <maheralkendi@Mahers-MacBook-Air.local>
2021-03-29 17:56:52 +0400

    Add a dialog box component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { TableTitleComponent } from './table-title/table-title.component';
 import { ViewProjectsComponent } from './view-projects/view-projects.component';
 import { ViewDeliverablesComponent } from './view-deliverables/view-deliverables.component';
 import { ProjectTableComponent } from './project-table/project-table.component';
+import { AddNewProjectDialogBox } from './project-table/project-table.component';
 import { DeliverableTableComponent } from './deliverable-table/deliverable-table.component'
 
 import { ProjectsService } from './projects.service';
@@ -26,7 +30,8 @@ import { DeliverablesService } from './deliverables.service';
     ViewProjectsComponent,
     ViewDeliverablesComponent,
     ProjectTableComponent,
-    DeliverableTableComponent
+    DeliverableTableComponent,
+    AddNewProjectDialogBox
   ],
   imports: [
     BrowserModule,
@@ -36,7 +41,10 @@ import { DeliverablesService } from './deliverables.service';
     MatCardModule,
     MatPaginatorModule,
     MatSortModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [ProjectsService, DeliverablesService],
   bootstrap: [AppComponent]
--- src/app/project-table/project-table.component.ts
+++ src/app/project-table/project-table.component.ts
@@ -1,7 +1,8 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
-import {MatSort} from '@angular/material/sort';
+import { Component, ViewChild, AfterViewInit, Inject } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSort } from '@angular/material/sort';
 import { IProject, ProjectsService } from '../projects.service';
 
 @Component({
@@ -12,24 +13,60 @@ import { IProject, ProjectsService } from '../projects.service';
 export class ProjectTableComponent implements AfterViewInit {
   displayedColumns: string[] = ['id', 'project', 'description', 'action'];
   dataSource = new MatTableDataSource<IProject>(this._projectsList.getProjects());
+  name: string = "test";
 
-  constructor(private _projectsList: ProjectsService) { }
+  constructor(private _projectsList: ProjectsService, public dialog: MatDialog) { }
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  ngAfterViewInit() {
+  ngAfterViewInit():void {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
-  openDialog() {
-    console.log("open")
-    this._projectsList.add().subscribe(data => {
-      console.log(data);
-      this.dataSource = new MatTableDataSource<IProject>(data);
+  openDialog():void {
+    const dialogRef = this.dialog.open(AddNewProjectDialogBox, {
+      width: '250px',
+      data: {name: this.name}
     });
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+
+    // console.log("open")
+    // this._projectsList.add().subscribe(data => {
+    //   console.log(data);
+    //   this.dataSource = new MatTableDataSource<IProject>(data);
+    // });
+    // this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
+  }
+}
+
+export interface DialogFormData {
+  projectName: string;
+  projectDescription: string;
+}
+
+@Component({
+  selector: 'add-project-dialog-box',
+  templateUrl: './add-new-project.component.html',
+  styleUrls: ['./add-new-project.component.css']
+})
+
+export class AddNewProjectDialogBox {
+  constructor(
+    public dialogRef: MatDialogRef<ProjectTableComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogFormData) {}
+
+    handleAddNewProjectFormSubmit():void {
+      console.log('submitted')
+      // console.log(x)
+    }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
